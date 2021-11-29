089697dd85e6daabe233cfe92729f55a4eae24b5
Zombie969 <57501509+Zombie969@users.noreply.github.com>
2021-11-29 17:49:53 +0300

    Add files via upload

---
+++ Productly/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1171,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+});
